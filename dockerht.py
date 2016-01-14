4af8c2f71ba535f468c27b7f26c26f0880e7d038
Dennis Brandenburg <dbranden@denniss-air-2.ad.here.com>
2016-01-14 10:03:15 +0100

    Apache 2 license added.

--- dockerht.py
+++ dockerht.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+# Copyright 2016 Dennis Brandenburg <d.brandenburg@db-network.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 import argparse
 import re
@@ -159,5 +173,4 @@ if __name__ == "__main__":
     vhost = args['vhost'][0]
     command = args['command'][0]
     dockerht.setup()
-    #command = "/usr/sbin/httpd -DFOREGROUND"
     dockerht.push_app(app_dir, vhost, command)
